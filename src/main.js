a582eefaea7d6334aff2a87cc97d3c3753acfdf4
王浩羽 <wanghaoyu@wanghaoyudeMBP.lan>
2020-08-14 22:39:53 +0800

    css样式打-上包

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import testImg from "./test-img.jpeg"
+import "./index.scss";
+
 
 const img = new Image();
 img.src = testImg;
+img.classList.add("image")
 let root = document.getElementById('root')
 root.appendChild(img)
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,32 @@
 const path = require('path')
 
 module.exports = {
-    mode: "development",
+    mode: "production",
     entry: {
         main: path.resolve(__dirname, "./src/main.js")
     },
     // 配置loader
     module: {
         rules: [{
-            test: /\.(jpe?g|png|gif)$/, // 匹配对应文件
-            use: {
-                // 对于匹配到的文件使用的处理loader
-                loader: "url-loader",
-                options: {
-                    // placeholder name ext hash ..
-                    name: "[name].[ext]",
-                    outputPath: "images/",
-                    // 小于200KB打包成base64个数
-                    limit: 204800
+                test: /\.(jpe?g|png|gif)$/, // 匹配对应文件
+                use: {
+                    // 对于匹配到的文件使用的处理loader
+                    loader: "url-loader",
+                    options: {
+                        // placeholder name ext hash ..
+                        name: "[name].[ext]",
+                        outputPath: "images/",
+                        // 小于200KB打包成base64个数
+                        limit: 204800
+                    }
                 }
+            },
+            {
+                test: /\.scss$/,
+                // css文件通常需要两个loader进行处理
+                use: ["style-loader", "css-loader","sass-loader","postcss-loader"]
             }
-        }]
+        ]
     },
     output: {
         filename: "bundle.js",
